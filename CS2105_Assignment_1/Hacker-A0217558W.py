578801d04509d8cdb805fe8d0af4d9289ee29b80
Wong Chee Hong <c.h.wong2606@gmail.com>
2022-02-13 19:31:47 +0800

    test

--- CS2105_Assignment_1/Hacker-A0217558W.py
+++ CS2105_Assignment_1/Hacker-A0217558W.py
@@ -1,4 +1,3 @@
-from http import client
 from socket import *
 import sys
 
@@ -6,12 +5,20 @@ targetServer = "137.132.92.111"
 serverPort = 4444
 
 def main():
+    print('test')
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((targetServer, serverPort))
-    studentId = sys.argv[1]
-    clientSocket.send()
+    print("connection established")
+    student_key = sys.argv[1]
+    handshake(student_key, clientSocket)
 
+def handshake(student_key, clientSocket):
+    message = ("STID_" + student_key).encode()
+    print("sending message: ", message)
+    clientSocket.send(message)
+    reply = clientSocket.recv(1024)
+    print("server:", reply)
+    
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
